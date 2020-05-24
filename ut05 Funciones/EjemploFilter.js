739c1e0b0b463b5dff4fabd3eaed3b54c80c8330
Ricardo Muñoz <36267396+Ricardoyecla@users.noreply.github.com>
2020-05-24 19:26:40 +0200

    Update EjemploFilter.js

--- ut05 Funciones/EjemploFilter.js
+++ ut05 Funciones/EjemploFilter.js
@@ -3,4 +3,11 @@
 
 const colores=["Óxido",,"Ámbar","azul",,"negro","Gris","Naranga","granate","oro"]; // Hay elementos indefinidos.
 
+/*function TamanaMayor6(elemento) {
+    return elemento.length>6
+}
+console.log(colores.filter(TamanaMayor6));
+*/
+
+
 console.log(colores.filter(color=>color.length<6));
